e1eb9aeef90edb78091d0dfdf31deaea5f687672
Walter Altamirano <altamiranow4@gmail.com>
2024-04-05 20:23:11 -0300

    Agrego Solucion con expresion Lambda del metodo "promedioEdadEquipo"

--- src/ar/edu/unq/poo2/tp3/EquipoDeTrabajo.java
+++ src/ar/edu/unq/poo2/tp3/EquipoDeTrabajo.java
@@ -3,15 +3,21 @@ package ar.edu.unq.poo2.tp3;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
+import java.util.Collection;
 public class EquipoDeTrabajo {
 	
 	private String nombreEquipo;
 	
-	private List<Persona> integrantes = new ArrayList<Persona>();
+	private Collection<Persona> integrantes = new ArrayList<Persona>();
 	
 	public EquipoDeTrabajo(String nombreEquipo) {
 		super();
 		this.nombreEquipo = nombreEquipo;
+	}
+	
+	protected Collection<Persona> getIntegrantes() {
+		return integrantes;
 	}
 	
 	public String nombreEquipo() {
@@ -25,24 +31,36 @@ public class EquipoDeTrabajo {
 	public void quitarPersona(Persona persona) {
 		integrantes.remove(persona);
 	}
-//	public List<Integer> edades() {
-//		return integrantes.map(persona -> persona.edad());
-//	}
-	
+
 	public int promedioEdadEquipo() {
 		
+		int sumatoriaDeEdades = this.getIntegrantes().stream()
+									 .mapToInt(persona -> persona.edad())
+									 .sum();
+		return sumatoriaDeEdades / integrantes.size();
+	}
+
+	/*
+	 *Otra solucion de promedioEdadEquipo()
+	 	
+	 	public int promedioEdadEquipo() {
+
 		List<Persona> personas = integrantes;
-		
+
 		int sumatoriaHastaAhora = 0;
-		
+
 		for(int i = 0; personas.size() != i ;i++) {
 			sumatoriaHastaAhora = sumatoriaHastaAhora + this.edadDe(personas.get(i));
 		}
-		
+
 		return sumatoriaHastaAhora / personas.size() ;
 	}
 
-	private int edadDe(Persona persona) {
-		return persona.edad();
-	}
+		private int edadDe(Persona persona) {
+			return persona.edad();
+		}
+	 
+	  */
 }
+
+
